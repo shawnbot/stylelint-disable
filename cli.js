3903d205faa7c056a155460992e8fa42520e6d9d
Shawn Allen <shawn.allen@github.com>
2019-10-24 01:54:48 -0700

    first commit

---
+++ cli.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+const minimist = require('minimist')
+const {writeFileSync} = require('fs')
+const stylelintDisable = require('.')
+
+const args = process.argv.slice(2)
+const options = minimist(args, {
+  '--': true,
+  boolean: ['dry-run', 'help'],
+  alias: {
+    n: 'dry-run',
+    h: 'help'
+  }
+})
+
+if (options.help) {
+  showUsage()
+}
+
+const rules = options._
+const files = options['--'] || []
+
+const dryRun = options['dry-run']
+
+if (rules.length === 0) {
+  console.warn(`No rules provided; disabling all rules!`)
+  rules.push('*')
+}
+
+if (files.length === 0) {
+  console.warn(`You must provide one or more files after "--" in the arguments list.`)
+  showUsage()
+}
+
+stylelintDisable({rules, files}).then(results => {
+  for (const {source, root, disabled} of results) {
+    console.warn(`Writing ${disabled} disables to ${source}...`)
+    if (!dryRun) {
+      writeFileSync(source, root.toString(), 'utf8')
+    }
+  }
+})
+
+function showUsage() {
+  const $0 = 'stylelint-only'
+  console.log(`
+${$0} [options] [rules] -- files
+
+Disable one or more rules by adding the requisite stylelint-disable comments.
+`)
+  process.exit(0)
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+const anymatch = require('anymatch')
+const postcss = require('postcss')
+const stylelint = require('stylelint')
+
+const DISABLE_PRAGMA = 'stylelint-disable-next-line'
+const DISABLE_COMMENT_PATTERN = new RegExp(`(${DISABLE_PRAGMA}) +(.+)`)
+
+module.exports = function stylelintDisable(options) {
+  const {rules = '*'} = options
+  const hasRule = anymatch(rules)
+  return stylelint.lint(options).then(data => {
+    const results = []
+
+    for (const {source, _postcssResult: result} of data.results) {
+      const {root} = result
+      const nodes = new Set()
+      let disabled = 0
+      for (const {rule, node, line} of result.warnings()) {
+        if (hasRule(rule)) {
+          console.warn(`+ disabling violation of ${rule} on ${source}:${line}`)
+          if (node.disables) {
+            node.disables.add(rule)
+          } else {
+            node.disables = new Set([rule])
+          }
+          disabled++
+          nodes.add(node)
+        } else {
+          console.warn(`- skipping violation of ${rule} on ${source}:${line}`)
+        }
+      }
+
+      if (nodes.size) {
+        for (const node of nodes) {
+          addDisableComment(node, node.disables)
+        }
+        results.push({
+          source,
+          root,
+          disabled
+        })
+      }
+    }
+
+    return results
+  })
+}
+
+function addDisableComment(node, rules) {
+  const {nodes} = node.parent
+  const previous = nodes[nodes.indexOf(node) - 1]
+  if (previous && previous.type === 'comment') {
+    const match = previous.text.match(DISABLE_COMMENT_PATTERN)
+    if (match) {
+      const [, pragma, existingRuleString] = match
+      const ruleSet = new Set(existingRules.split(/ *, */).concat(rules))
+      previous.text = `${pragma} ${Array.from(ruleSet).join(', ')}`
+      // console.warn(`updated comment: "${previous.text}"`)
+      return
+    } else {
+      // console.warn(`previous comment is not a disable: "${previous.text}"`)
+    }
+  }
+  const comment = postcss.comment({
+    text: `${DISABLE_PRAGMA} ${Array.from(rules).join(', ')}`,
+    raws: {
+      inline: true,
+      before: node.raws.before,
+      after: '\n'
+    }
+  })
+  // console.warn(`inserted: "${comment.text}"`)
+  node.parent.insertBefore(node, comment)
+}
